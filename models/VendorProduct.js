ad4733dc4990121575b0b37129ee06a9f562e6bc
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-17 13:57:53 +0530

    Update VendorProduct.js

--- models/VendorProduct.js
+++ models/VendorProduct.js
@@ -197,6 +197,55 @@
 
 
 
+// const mongoose = require("mongoose");
+
+// const VendorProductSchema = new mongoose.Schema({
+//   name: String,
+//   brandName: String,
+//   phone: String,
+//   contactName: String,
+//   description: String,
+
+//   oldPrice: Number,
+//   newPrice: Number,
+//   stock: Number,
+//   quality: String,
+
+//   State: String,
+//   productTypes: String,
+//   flavors: String,
+//   dietPreference: String,
+//   nutrition: String,
+//   materialTypes: String,
+//   ingredients: String,
+//   allergenInfo: String,
+//   dietaryPreferences: String,
+//   cuisine: String,
+//   size: String,
+//   storeName: { type: String, default: "" },
+
+
+//   image: String,
+//   logo: String,
+//   gallery: [String],
+
+//   category: { type: mongoose.Schema.Types.ObjectId, ref: "VendorCategory" },
+//   subcategory: { type: mongoose.Schema.Types.ObjectId, ref: "VendorSubCategory" },
+
+//   restaurantName: String,
+
+//   vendor: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "Vendor",
+//     required: true,
+//   },
+// }, { timestamps: true });
+
+// module.exports = mongoose.model("VendorProduct", VendorProductSchema);
+
+
+
+
 const mongoose = require("mongoose");
 
 const VendorProductSchema = new mongoose.Schema({
