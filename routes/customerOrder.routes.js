1c78bc64157e38a5b112cee8669b8916397a0b8d
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-19 15:21:34 +0530

    Update customerOrder.routes.js

--- routes/customerOrder.routes.js
+++ routes/customerOrder.routes.js
@@ -17,6 +17,39 @@
 
 
 
+// const express = require("express");
+// const router = express.Router();
+
+// const {
+//   createOrder,
+//   getCustomerOrders,
+//   getOrderById,
+//   cancelOrder,
+//   getVendorOrders,
+// } = require("../controllers/customerOrder.controller");
+
+// router.post("/order/create", createOrder);
+
+// router.get("/orders/:customerId", getCustomerOrders);
+
+// router.get("/order/:id", getOrderById);
+
+// router.put("/order/cancel/:id", cancelOrder);
+
+// router.get("/vendor-orders/:vendorId", getVendorOrders);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
 const express = require("express");
 const router = express.Router();
 
@@ -26,16 +59,19 @@ const {
   getOrderById,
   cancelOrder,
   getVendorOrders,
+  getAllOrdersForAdmin,
 } = require("../controllers/customerOrder.controller");
 
+/* ========== CUSTOMER ========== */
 router.post("/order/create", createOrder);
-
 router.get("/orders/:customerId", getCustomerOrders);
-
 router.get("/order/:id", getOrderById);
-
 router.put("/order/cancel/:id", cancelOrder);
 
+/* ========== VENDOR ========== */
 router.get("/vendor-orders/:vendorId", getVendorOrders);
+
+/* ========== ADMIN ========== */
+router.get("/admin/orders", getAllOrdersForAdmin);
 
 module.exports = router;
