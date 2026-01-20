4147b7aeba275dded42b81d128df3848af208e66
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-20 11:36:14 +0530

    Update razorpay.js

--- config/razorpay.js
+++ config/razorpay.js
@@ -8,18 +8,11 @@
 // module.exports = razorpay;
 
 
-
-
-
 const Razorpay = require("razorpay");
 
-if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
-  throw new Error("Razorpay keys are missing in environment variables");
-}
-
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID.trim(),
-  key_secret: process.env.RAZORPAY_KEY_SECRET.trim(),
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 module.exports = razorpay;
