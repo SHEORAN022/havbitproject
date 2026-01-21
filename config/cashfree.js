6e9dd7c9bec36a1e2c6ad51355dd41fe00295977
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-21 15:17:10 +0530

    Create cashfree.js

---
+++ config/cashfree.js
@@ -0,0 +1,14 @@
+const { Cashfree } = require("cashfree-pg");
+
+
+
+Cashfree.XClientId = process.env.CASHFREE_APP_ID;
+Cashfree.XClientSecret = process.env.CASHFREE_SECRET_KEY;
+
+
+Cashfree.XEnvironment =
+  process.env.CASHFREE_ENV === "PROD"
+    ? "PRODUCTION"
+    : "SANDBOX";
+
+module.exports = Cashfree;
