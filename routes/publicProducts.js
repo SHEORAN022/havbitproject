b75fcf40413f91716c91d6b90bc8bcf361345616
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-21 16:17:21 +0530

    Update publicProducts.js

--- routes/publicProducts.js
+++ routes/publicProducts.js
@@ -1,6 +1,17 @@
+// const router = require("express").Router();
+// const { getPublicProducts } = require("../controllers/publicProductController");
+
+// router.get("/products", getPublicProducts);
+
+// module.exports = router;
+
+
+
+
 const router = require("express").Router();
 const { getPublicProducts } = require("../controllers/publicProductController");
 
 router.get("/products", getPublicProducts);
 
 module.exports = router;
+
