02f3ed9cf7c0c8106be551d172b8fd6c5ad456a1
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-21 15:18:43 +0530

    Update razorpay.js

--- config/razorpay.js
+++ config/razorpay.js
@@ -8,6 +8,16 @@
 // module.exports = razorpay;
 
 
+// const Razorpay = require("razorpay");
+
+// const razorpay = new Razorpay({
+//   key_id: process.env.RAZORPAY_KEY_ID,
+//   key_secret: process.env.RAZORPAY_KEY_SECRET,
+// });
+
+// module.exports = razorpay;
+
+
 const Razorpay = require("razorpay");
 
 const razorpay = new Razorpay({
@@ -16,3 +26,4 @@ const razorpay = new Razorpay({
 });
 
 module.exports = razorpay;
+
