1fbd0e5bb0a21826b7da324bd3ab6a0a5854138a
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-21 16:24:43 +0530

    Update vendorCategoryRoutes.js

--- routes/vendorCategoryRoutes.js
+++ routes/vendorCategoryRoutes.js
@@ -1,3 +1,41 @@
+// const express = require("express");
+// const router = express.Router();
+// const cloudUpload = require("../middleware/cloudUpload");
+// const vendorAuth = require("../middleware/vendorAuth");
+
+// const {
+//   createVendorCategory,
+//   getVendorCategories,
+//   getVendorCategoryById,
+//   updateVendorCategory,
+//   deleteVendorCategory
+// } = require("../controllers/vendorCategoryController");
+
+// /* Vendor Protected Routes */
+// router.use(vendorAuth);
+
+// // CREATE
+// router.post("/", cloudUpload.single("image"), createVendorCategory);
+
+// // READ ALL
+// router.get("/", getVendorCategories);
+
+// // READ SINGLE
+// router.get("/:id", getVendorCategoryById);
+
+// // UPDATE
+// router.put("/:id", cloudUpload.single("image"), updateVendorCategory);
+
+// // DELETE
+// router.delete("/:id", deleteVendorCategory);
+
+// module.exports = router;
+
+
+
+
+
+
 const express = require("express");
 const router = express.Router();
 const cloudUpload = require("../middleware/cloudUpload");
@@ -30,3 +68,4 @@ router.put("/:id", cloudUpload.single("image"), updateVendorCategory);
 router.delete("/:id", deleteVendorCategory);
 
 module.exports = router;
+
