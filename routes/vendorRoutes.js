b7da5ae44afa6ca625278f26d6af73518e196619
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-21 16:27:27 +0530

    Update vendorRoutes.js

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,4 +1,29 @@
 
+// const express = require("express");
+// const router = express.Router();
+
+// const vendorAuth = require("../middleware/vendorAuth");
+// const productCtrl = require("../controllers/vendorProductController");
+// const subCtrl = require("../controllers/vendorSubCategoryController");
+// const catCtrl = require("../controllers/vendorCategoryController");
+
+// router.get("/categories", vendorAuth, catCtrl.getVendorCategories);
+// router.get("/subcategories", vendorAuth, subCtrl.getVendorSubCategories);
+
+// router.get("/products", vendorAuth, productCtrl.getVendorProducts);
+// router.post("/products", vendorAuth, productCtrl.createVendorProduct);
+// router.put("/products/:id", vendorAuth, productCtrl.updateVendorProduct);
+// router.delete("/products/:id", vendorAuth, productCtrl.deleteVendorProduct);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
 const express = require("express");
 const router = express.Router();
 
