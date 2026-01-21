0b1b4598c431f582269c08ddb8c6ab6bdc152c93
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-21 16:11:20 +0530

    Update adminCustomerRoutes.js

--- routes/adminCustomerRoutes.js
+++ routes/adminCustomerRoutes.js
@@ -1,5 +1,23 @@
 
 
+// const express = require("express");
+// const router = express.Router();
+// const adminCustomer = require("../controllers/adminCustomerController");
+
+// // Admin Customer CRUD
+// router.get("/", adminCustomer.getCustomers);
+// router.post("/", adminCustomer.addCustomer);
+// router.put("/:id", adminCustomer.updateCustomer);
+// router.delete("/:id", adminCustomer.deleteCustomer);
+
+// module.exports = router;
+
+
+
+
+
+
+
 const express = require("express");
 const router = express.Router();
 const adminCustomer = require("../controllers/adminCustomerController");
@@ -11,3 +29,4 @@ router.put("/:id", adminCustomer.updateCustomer);
 router.delete("/:id", adminCustomer.deleteCustomer);
 
 module.exports = router;
+
