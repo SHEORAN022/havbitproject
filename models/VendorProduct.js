320828ca3553a581ec3e655dd04ff7e7a8a24201
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-21 17:54:09 +0530

    Update VendorProduct.js

--- models/VendorProduct.js
+++ models/VendorProduct.js
@@ -246,102 +246,50 @@
 
 
 
-// const mongoose = require("mongoose");
-
-// const VendorProductSchema = new mongoose.Schema({
-//   name: String,
-//   brandName: String,
-//   phone: String,
-//   contactName: String,
-//   description: String,
-
-//   oldPrice: Number,
-//   newPrice: Number,
-//   stock: Number,
-//   quality: String,
-
-//   State: String,
-//   productTypes: String,
-//   flavors: String,
-//   dietPreference: String,
-//   nutrition: String,
-//   materialTypes: String,
-//   ingredients: String,
-//   allergenInfo: String,
-//   dietaryPreferences: String,
-//   cuisine: String,
-//   size: String,
-//   storeName: { type: String, default: "" },
-
-
-//   image: String,
-//   logo: String,
-//   gallery: [String],
-
-//   category: { type: mongoose.Schema.Types.ObjectId, ref: "VendorCategory" },
-//   subcategory: { type: mongoose.Schema.Types.ObjectId, ref: "VendorSubCategory" },
-
-//   restaurantName: String,
-
-//   vendor: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Vendor",
-//     required: true,
-//   },
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("VendorProduct", VendorProductSchema);
-
-
-
-
-
-
-
-
-
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 
-const VendorSchema = new mongoose.Schema({
+const VendorProductSchema = new mongoose.Schema({
   name: String,
-  email: String,
+  brandName: String,
   phone: String,
-  password: String,
-  
-  // YEH FIELD ADD KARO:
-  storeName: {
-    type: String,
-    default: "",
-    trim: true
+  contactName: String,
+  description: String,
+
+  oldPrice: Number,
+  newPrice: Number,
+  stock: Number,
+  quality: String,
+
+  State: String,
+  productTypes: String,
+  flavors: String,
+  dietPreference: String,
+  nutrition: String,
+  materialTypes: String,
+  ingredients: String,
+  allergenInfo: String,
+  dietaryPreferences: String,
+  cuisine: String,
+  size: String,
+  storeName: { type: String, default: "" },
+
+
+  image: String,
+  logo: String,
+  gallery: [String],
+
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "VendorCategory" },
+  subcategory: { type: mongoose.Schema.Types.ObjectId, ref: "VendorSubCategory" },
+
+  restaurantName: String,
+
+  vendor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Vendor",
+    required: true,
   },
-  
-  fssaiLicense: String,
-  gstNumber: String,
-  address: String,
-  city: String,
-  state: String,
-  pincode: String,
-  
-  isApproved: { type: Boolean, default: false },
-  isActive: { type: Boolean, default: true },
-  
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
-  
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
-});
+}, { timestamps: true });
 
-VendorSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
+module.exports = mongoose.model("VendorProduct", VendorProductSchema);
 
-VendorSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.password);
-};
 
-module.exports = mongoose.model("Vendor", VendorSchema);
