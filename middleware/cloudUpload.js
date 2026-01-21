8b2b9aadda1d41b6127fe0cbf25e546b384a10d9
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-21 15:48:30 +0530

    Update cloudUpload.js

--- middleware/cloudUpload.js
+++ middleware/cloudUpload.js
@@ -2,6 +2,35 @@
 
 
 
+// const multer = require("multer");
+// const { CloudinaryStorage } = require("multer-storage-cloudinary");
+// const cloudinary = require("../config/cloudinary");
+
+// // FIXED — Only one params object
+// const storage = new CloudinaryStorage({
+//   cloudinary,
+//   params: {
+//     folder: "products",
+//     allowed_formats: ["jpg", "jpeg", "png", "webp"],
+//     transformation: [{ width: 800, height: 800, crop: "limit" }],
+//   }
+// });
+
+// const cloudUpload = multer({ storage });
+
+// module.exports = cloudUpload;
+
+
+
+
+
+
+
+
+
+
+
+
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
@@ -19,3 +48,4 @@ const storage = new CloudinaryStorage({
 const cloudUpload = multer({ storage });
 
 module.exports = cloudUpload;
+
