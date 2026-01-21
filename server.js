f6e110bd93645578466932f798c827347436815e
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-21 18:21:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -374,6 +374,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+       "https://havbit.in",
       
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
