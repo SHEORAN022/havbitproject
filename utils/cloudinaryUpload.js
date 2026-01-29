da0473ad68bb36dbd61a584b82343926ff94aa94
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-29 18:10:00 +0530

    Create cloudinaryUpload.js

---
+++ utils/cloudinaryUpload.js
@@ -0,0 +1,18 @@
+const cloudinary = require("../config/cloudinary");
+
+const uploadToCloudinary = (buffer, folder = "products") => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload_stream(
+      {
+        folder,
+        resource_type: "image",
+      },
+      (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      }
+    ).end(buffer);
+  });
+};
+
+module.exports = uploadToCloudinary;
