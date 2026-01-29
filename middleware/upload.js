d8b49aac4ceec3e0a8779032a4521d84fa978173
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-29 18:01:33 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -55,9 +55,39 @@
 
 
 
+// const multer = require("multer");
+
+// module.exports = multer({
+//   storage: multer.memoryStorage(),
+//   limits: { fileSize: 50 * 1024 * 1024 },
+// });
+
+
+
+
+
+
 const multer = require("multer");
 
-module.exports = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 50 * 1024 * 1024 },
+// memory storage (Cloudinary ke liye BEST)
+const storage = multer.memoryStorage();
+
+// file filter (sirf image allow)
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
 });
+
+module.exports = upload;
+
