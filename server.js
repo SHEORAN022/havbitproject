9175ca43b6b8937c676608027aff53ba81fe13c1
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-30 19:22:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -454,6 +454,7 @@ app.use("/api/vendor/media", require("./routes/restaurantMediaRoutes"));
 app.use("/api/vendor/orders", vendorOrderRoutes);
 app.use("/api/vendor", require("./routes/vendorBrandRoutes"));
 app.use("/api/vendor/inventory", require("./routes/inventoryRoutes"));
+app.use("/api", require("./routes/publicBrandRoutes"));
 
 /* ADMIN */
 app.use("/api/admin/vendors", require("./routes/adminVendor"));
