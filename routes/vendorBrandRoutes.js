18e808b51789ad18bb06879b0ea7ff5ce0debaa8
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-30 19:20:48 +0530

    Update vendorBrandRoutes.js

--- routes/vendorBrandRoutes.js
+++ routes/vendorBrandRoutes.js
@@ -77,6 +77,47 @@
 
 
 
+// const router = require("express").Router();
+// const upload = require("../middleware/upload");
+// const vendorAuth = require("../middleware/vendorAuth");
+
+// const {
+//   createBrand,
+//   getBrand,
+//   updateBrand,
+//   deleteBrand,
+// } = require("../controllers/vendorBrandController");
+
+// router.post(
+//   "/brand",
+//   vendorAuth,
+//   upload.fields([
+//     { name: "logo", maxCount: 1 },
+//     { name: "banner", maxCount: 1 },
+//   ]),
+//   createBrand
+// );
+
+// router.get("/brand", vendorAuth, getBrand);
+
+// router.put(
+//   "/brand",
+//   vendorAuth,
+//   upload.fields([
+//     { name: "logo", maxCount: 1 },
+//     { name: "banner", maxCount: 1 },
+//   ]),
+//   updateBrand
+// );
+
+// router.delete("/brand", vendorAuth, deleteBrand);
+
+// module.exports = router;
+
+
+
+
+
 const router = require("express").Router();
 const upload = require("../middleware/upload");
 const vendorAuth = require("../middleware/vendorAuth");
@@ -113,4 +154,5 @@ router.put(
 router.delete("/brand", vendorAuth, deleteBrand);
 
 module.exports = router;
+
 
