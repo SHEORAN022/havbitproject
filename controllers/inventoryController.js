aed32b2669435d73bec17b5e7d199ae0e9a85945
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-30 17:56:12 +0530

    Update inventoryController.js

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,9 +1,99 @@
+// // const Inventory = require("../models/Inventory");
+
+// // /* CREATE / INIT INVENTORY */
+// // exports.createInventory = async (req, res) => {
+// //   try {
+// //     const { product, stock, lowStockAlert } = req.body;
+
+// //     const inventory = await Inventory.create({
+// //       vendor: req.vendor._id,
+// //       product,
+// //       stock,
+// //       lowStockAlert,
+// //     });
+
+// //     res.status(201).json({ success: true, inventory });
+// //   } catch (err) {
+// //     res.status(500).json({ success: false, message: err.message });
+// //   }
+// // };
+
+// // /* GET ALL INVENTORY (VENDOR) */
+// // exports.getMyInventory = async (req, res) => {
+// //   try {
+// //     const inventory = await Inventory.find({
+// //       vendor: req.vendor._id,
+// //     }).populate("product", "name price image");
+
+// //     res.json({ success: true, inventory });
+// //   } catch (err) {
+// //     res.status(500).json({ message: err.message });
+// //   }
+// // };
+
+// // /* UPDATE STOCK */
+// // exports.updateStock = async (req, res) => {
+// //   try {
+// //     const { stock } = req.body;
+
+// //     const inventory = await Inventory.findOne({
+// //       product: req.params.productId,
+// //       vendor: req.vendor._id,
+// //     });
+
+// //     if (!inventory)
+// //       return res.status(404).json({ message: "Inventory not found" });
+
+// //     inventory.stock = stock;
+// //     await inventory.save();
+
+// //     res.json({ success: true, inventory });
+// //   } catch (err) {
+// //     res.status(500).json({ message: err.message });
+// //   }
+// // };
+
+// // /* 🔥 DECREASE STOCK (ORDER TIME) */
+// // exports.decreaseStock = async (productId, qty) => {
+// //   const inventory = await Inventory.findOne({ product: productId });
+
+// //   if (!inventory || inventory.stock < qty) {
+// //     throw new Error("Insufficient stock");
+// //   }
+
+// //   inventory.stock -= qty;
+// //   await inventory.save();
+// // };
+
+
+
+
+
 // const Inventory = require("../models/Inventory");
 
-// /* CREATE / INIT INVENTORY */
+// /* ================= CREATE ================= */
 // exports.createInventory = async (req, res) => {
 //   try {
 //     const { product, stock, lowStockAlert } = req.body;
+
+//     if (!product || stock === undefined) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "Product and stock required",
+//       });
+//     }
+
+//     const already = await Inventory.findOne({
+//       vendor: req.vendor._id,
+//       product,
+//     });
+
+//     if (already) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "Inventory already exists",
+//       });
+//     }
 
 //     const inventory = await Inventory.create({
 //       vendor: req.vendor._id,
@@ -18,7 +108,7 @@
 //   }
 // };
 
-// /* GET ALL INVENTORY (VENDOR) */
+// /* ================= READ ================= */
 // exports.getMyInventory = async (req, res) => {
 //   try {
 //     const inventory = await Inventory.find({
@@ -27,35 +117,66 @@
 
 //     res.json({ success: true, inventory });
 //   } catch (err) {
-//     res.status(500).json({ message: err.message });
+//     res.status(500).json({ success: false, message: err.message });
 //   }
 // };
 
-// /* UPDATE STOCK */
-// exports.updateStock = async (req, res) => {
+// /* ================= UPDATE ================= */
+// exports.updateInventory = async (req, res) => {
 //   try {
-//     const { stock } = req.body;
+//     const { stock, lowStockAlert } = req.body;
 
 //     const inventory = await Inventory.findOne({
-//       product: req.params.productId,
+//       _id: req.params.id,
 //       vendor: req.vendor._id,
 //     });
 
-//     if (!inventory)
-//       return res.status(404).json({ message: "Inventory not found" });
+//     if (!inventory) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Inventory not found",
+//       });
+//     }
 
-//     inventory.stock = stock;
+//     if (stock !== undefined) inventory.stock = stock;
+//     if (lowStockAlert !== undefined)
+//       inventory.lowStockAlert = lowStockAlert;
+
 //     await inventory.save();
 
 //     res.json({ success: true, inventory });
 //   } catch (err) {
-//     res.status(500).json({ message: err.message });
+//     res.status(500).json({ success: false, message: err.message });
 //   }
 // };
 
-// /* 🔥 DECREASE STOCK (ORDER TIME) */
-// exports.decreaseStock = async (productId, qty) => {
-//   const inventory = await Inventory.findOne({ product: productId });
+// /* ================= DELETE ================= */
+// exports.deleteInventory = async (req, res) => {
+//   try {
+//     const inventory = await Inventory.findOneAndDelete({
+//       _id: req.params.id,
+//       vendor: req.vendor._id,
+//     });
+
+//     if (!inventory) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Inventory not found",
+//       });
+//     }
+
+//     res.json({ success: true, message: "Inventory deleted" });
+//   } catch (err) {
+//     res.status(500).json({ success: false, message: err.message });
+//   }
+// };
+
+// /* ================= ORDER TIME STOCK CUT ================= */
+// exports.decreaseStock = async (productId, vendorId, qty) => {
+//   const inventory = await Inventory.findOne({
+//     product: productId,
+//     vendor: vendorId,
+//   });
 
 //   if (!inventory || inventory.stock < qty) {
 //     throw new Error("Insufficient stock");
@@ -64,7 +185,6 @@
 //   inventory.stock -= qty;
 //   await inventory.save();
 // };
-
 
 
 
