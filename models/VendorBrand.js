6fb00acfa31e1a6de554831769cbfe6e72bc8337
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-30 19:17:20 +0530

    Update VendorBrand.js

--- models/VendorBrand.js
+++ models/VendorBrand.js
@@ -1,3 +1,36 @@
+// const mongoose = require("mongoose");
+
+// const vendorBrandSchema = new mongoose.Schema(
+//   {
+//     vendor: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Vendor",
+//       required: true,
+//       unique: true,
+//     },
+//     brandName: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     logo: {
+//       url: String,
+//       public_id: String,
+//     },
+//     banner: {
+//       url: String,
+//       public_id: String,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("VendorBrand", vendorBrandSchema);
+
+
+
+
+// module.exports = mongoose.model("VendorBrand", vendorBrandSchema);
 const mongoose = require("mongoose");
 
 const vendorBrandSchema = new mongoose.Schema(
@@ -26,3 +59,4 @@ const vendorBrandSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("VendorBrand", vendorBrandSchema);
+
