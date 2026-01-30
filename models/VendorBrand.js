3500bf4aeeca392c161c7a2fad4ce81a95512483
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-30 15:32:37 +0530

    Create VendorBrand.js

---
+++ models/VendorBrand.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const vendorBrandSchema = new mongoose.Schema(
+  {
+    vendor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
+      unique: true,
+    },
+    brandName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    logo: {
+      url: String,
+      public_id: String,
+    },
+    banner: {
+      url: String,
+      public_id: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("VendorBrand", vendorBrandSchema);
