de4673d0903d216fd2c82195c0e04f396d63e493
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-31 12:47:54 +0530

    Update publicBrandRoutes.js

--- routes/publicBrandRoutes.js
+++ routes/publicBrandRoutes.js
@@ -1,8 +1,34 @@
-const router = require("express").Router();
+// const router = require("express").Router();
+// const {
+//   getPublicBrandByVendorId,
+// } = require("../controllers/vendorBrandController");
+
+// router.get("/brands/:vendorId", getPublicBrandByVendorId);
+
+// module.exports = router;
+
+
+
+
+
+const express = require("express");
+const router = express.Router();
+
 const {
+  getAllPublicBrands,
   getPublicBrandByVendorId,
 } = require("../controllers/vendorBrandController");
 
+/**
+ * 🌍 WEBSITE
+ * Get all vendors brands
+ */
+router.get("/brands", getAllPublicBrands);
+
+/**
+ * 🌍 WEBSITE
+ * Get single vendor brand by vendorId
+ */
 router.get("/brands/:vendorId", getPublicBrandByVendorId);
 
 module.exports = router;
