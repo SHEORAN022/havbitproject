54502ed23e14fc4a95a39e2397db21c2d39f44fd
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-02 14:26:04 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,107 +1,49 @@
 
 
-// const router = require("express").Router();
-// const upload = require("../middleware/upload");
-
-// const {
-//   getProducts,
-//   getProductById,
-//   addProduct,
-//   updateProduct,
-//   deleteProduct,
-//   bulkDelete,
-//   bulkUpdate,
-// } = require("../controllers/productController");
-
-// router.get("/", getProducts);
-// router.get("/:id", getProductById);
-
-// router.post(
-//   "/",
-//   upload.fields([
-//     { name: "image", maxCount: 1 },
-//     { name: "mandatoryImages.ingredientsImage", maxCount: 1 },
-//     { name: "mandatoryImages.nutritionImage", maxCount: 1 },
-//     { name: "mandatoryImages.mfgExpImage", maxCount: 1 },
-//     { name: "gallery", maxCount: 10 },
-//   ]),
-//   addProduct
-// );
-
-// router.put(
-//   "/:id",
-//   upload.fields([
-//     { name: "image", maxCount: 1 },
-//     { name: "mandatoryImages.ingredientsImage", maxCount: 1 },
-//     { name: "mandatoryImages.nutritionImage", maxCount: 1 },
-//     { name: "mandatoryImages.mfgExpImage", maxCount: 1 },
-//     { name: "gallery", maxCount: 10 },
-//   ]),
-//   updateProduct
-// );
-
-// router.delete("/:id", deleteProduct);
-// router.post("/bulk-delete", bulkDelete);
-// router.put("/bulk-update", bulkUpdate);
-
-// module.exports = router;
-
-
-
-
-
-
 const router = require("express").Router();
-const { handleProductUpload, handleVariationUpload } = require("../middleware/upload");
+const upload = require("../middleware/upload");
 
 const {
   getProducts,
   getProductById,
-  getProductBySlug,
   addProduct,
   updateProduct,
   deleteProduct,
   bulkDelete,
   bulkUpdate,
-  addVariation,
-  updateVariation,
-  deleteVariation,
-  getProductVariations,
-  searchProducts,
-  getFeaturedProducts,
-  getProductsByCategory,
-  uploadProductImages,
-  uploadVariationImages,
-  deleteProductImage,
-  deleteVariationImage
 } = require("../controllers/productController");
 
-// Public routes
 router.get("/", getProducts);
-router.get("/featured", getFeaturedProducts);
-router.get("/search", searchProducts);
-router.get("/category/:categoryId", getProductsByCategory);
-router.get("/slug/:slug", getProductBySlug);
 router.get("/:id", getProductById);
-router.get("/:id/variations", getProductVariations);
 
-// Admin routes
-router.post("/", handleProductUpload, addProduct);
-router.put("/:id", handleProductUpload, updateProduct);
+router.post(
+  "/",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "mandatoryImages.ingredientsImage", maxCount: 1 },
+    { name: "mandatoryImages.nutritionImage", maxCount: 1 },
+    { name: "mandatoryImages.mfgExpImage", maxCount: 1 },
+    { name: "gallery", maxCount: 10 },
+  ]),
+  addProduct
+);
 
-// Variation management
-router.post("/:id/variations", handleVariationUpload, addVariation);
-router.put("/:id/variations/:variationId", handleVariationUpload, updateVariation);
-router.delete("/:id/variations/:variationId", deleteVariation);
+router.put(
+  "/:id",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "mandatoryImages.ingredientsImage", maxCount: 1 },
+    { name: "mandatoryImages.nutritionImage", maxCount: 1 },
+    { name: "mandatoryImages.mfgExpImage", maxCount: 1 },
+    { name: "gallery", maxCount: 10 },
+  ]),
+  updateProduct
+);
 
-// Image management
-router.post("/:id/images", upload.array("images", 12), uploadProductImages);
-router.delete("/:id/images/:imageIndex", deleteProductImage);
-router.post("/:id/variations/:variationId/images", upload.array("images", 12), uploadVariationImages);
-router.delete("/:id/variations/:variationId/images/:imageIndex", deleteVariationImage);
-
-// Bulk operations
-router.delete("/", bulkDelete);
+router.delete("/:id", deleteProduct);
+router.post("/bulk-delete", bulkDelete);
 router.put("/bulk-update", bulkUpdate);
 
 module.exports = router;
+
+
