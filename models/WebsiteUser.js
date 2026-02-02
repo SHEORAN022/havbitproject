4675f79a742085faa61b987a395c6963fc4a2e53
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-02 19:31:38 +0530

    Update WebsiteUser.js

--- models/WebsiteUser.js
+++ models/WebsiteUser.js
@@ -1,49 +1,3 @@
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs");
-
-// const websiteUserSchema = new mongoose.Schema(
-//   {
-//     fullName: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//     },
-
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       lowercase: true,
-//       trim: true,
-//     },
-
-//     phone: {
-//       type: String,
-//       required: true,
-//     },
-
-//     password: {
-//       type: String,
-//       required: true,
-//       minlength: 6,
-//       select: false,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// // 🔐 hash password
-// websiteUserSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
-//   this.password = await bcrypt.hash(this.password, 10);
-//   next();
-// });
-
-// module.exports = mongoose.model("WebsiteUser", websiteUserSchema);
-
-
-
-
 
 // const mongoose = require("mongoose");
 // const bcrypt = require("bcryptjs");
@@ -79,17 +33,6 @@
 
 // module.exports = mongoose.model("WebsiteUser", websiteUserSchema);
 
-
-
-
-
-
-
-
-
-
-
-
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
@@ -102,22 +45,28 @@ const websiteUserSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
-      select: true,
     },
-    phone: { type: String, required: true },
+    phone: { type: String, required: false }, // Make optional for Google login
     password: {
       type: String,
-      required: true,
+      required: false, // Make optional for Google login
       minlength: 6,
       select: false,
     },
+    authProvider: {
+      type: String,
+      enum: ["local", "google"],
+      default: "local",
+    },
+    googleId: { type: String, unique: true, sparse: true },
+    profilePicture: { type: String },
   },
   { timestamps: true }
 );
 
-// 🔐 hash password
+// 🔐 hash password (only for local auth)
 websiteUserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+  if (!this.isModified("password") || !this.password) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
