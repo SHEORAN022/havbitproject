2b8ca2fd62e35a963e73d7298390cbd2b38d4bed
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-03 12:36:36 +0530

    Update vendorOrderRoutes.js

--- routes/vendorOrderRoutes.js
+++ routes/vendorOrderRoutes.js
@@ -58,6 +58,33 @@
 
 
 
+// const express = require("express");
+// const router = express.Router();
+
+// const vendorAuth = require("../middleware/vendorAuth");
+
+// const {
+//   getVendorOrders,
+//   getVendorOrderById,
+//   updateVendorOrderStatus,
+//   getVendorOrderStats,
+// } = require("../controllers/vendorOrderController");
+
+// router.use(vendorAuth);
+
+// /* ================= VENDOR ORDERS ================= */
+// router.get("/", getVendorOrders);              // all vendor orders
+// router.get("/stats", getVendorOrderStats);     // stats
+// router.get("/:id", getVendorOrderById);        // single order
+// router.put("/:id/status", updateVendorOrderStatus); // update status
+
+// module.exports = router;
+
+
+
+
+
+
 const express = require("express");
 const router = express.Router();
 
@@ -68,6 +95,7 @@ const {
   getVendorOrderById,
   updateVendorOrderStatus,
   getVendorOrderStats,
+  createVendorOrder,
 } = require("../controllers/vendorOrderController");
 
 router.use(vendorAuth);
@@ -77,6 +105,8 @@ router.get("/", getVendorOrders);              // all vendor orders
 router.get("/stats", getVendorOrderStats);     // stats
 router.get("/:id", getVendorOrderById);        // single order
 router.put("/:id/status", updateVendorOrderStatus); // update status
+router.post("/create", createVendorOrder);
 
 module.exports = router;
+
 
