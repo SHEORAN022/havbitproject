eccdcabb18e11cdad22018b9631825168eddffa4
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-07 14:41:29 +0530

    Update customerOrder.routes.js

--- routes/customerOrder.routes.js
+++ routes/customerOrder.routes.js
@@ -1,69 +1,3 @@
-
-
-
-
-
-
-// // const express = require("express");
-// // const router = express.Router();
-
-// // const {
-// //   createOrder,
-// //   getCustomerOrders,
-// //   getOrderById,
-// //   cancelOrder,
-// //   updateOrderStatus,
-// //   getVendorOrders,
-// //   getAllOrdersForAdmin,
-// // } = require("../controllers/customerOrder.controller");
-
-// // router.post("/order/create", createOrder);
-// // router.get("/orders/:customerId", getCustomerOrders);
-// // router.get("/order/:id", getOrderById);
-// // router.put("/order/cancel/:id", cancelOrder);
-// // router.put("/order/status/:id", updateOrderStatus);
-// // router.get("/vendor-orders/:vendorId", getVendorOrders);
-// // router.get("/admin/orders", getAllOrdersForAdmin);
-
-// // module.exports = router;
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const router = express.Router();
-
-// const {
-//   createOrder,
-//   getCustomerOrders,
-//   getOrderById,
-//     getMyOrders, 
-//   cancelOrder,
-//   updateOrderStatus,
-//   getVendorOrders,
-//   getAllOrdersForAdmin,
-//   deleteOrder,
-// } = require("../controllers/customerOrder.controller");
-
-// router.post("/order/create", createOrder);
-// router.get("/orders/:customerId", getCustomerOrders);
-// router.get("/my-orders", customerAuth, getMyOrders);
-// router.get("/order/:id", getOrderById);
-// router.put("/order/cancel/:id", cancelOrder);
-// router.put("/order/status/:id", updateOrderStatus);
-// // router.put("/order/status/:id", updateOrderStatus);
-// router.get("/vendor-orders/:vendorId", getVendorOrders);
-// router.get("/admin/orders", getAllOrdersForAdmin);
-// router.delete("/order/:id", deleteOrder);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 
@@ -96,4 +30,3 @@ router.get("/admin/orders", getAllOrdersForAdmin);
 router.delete("/order/:id", deleteOrder);
 
 module.exports = router;
-
