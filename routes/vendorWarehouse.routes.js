c3b42520711603b303668863fc3f05bad8d65c4d
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-11 13:32:19 +0530

    Update vendorWarehouse.routes.js

--- routes/vendorWarehouse.routes.js
+++ routes/vendorWarehouse.routes.js
@@ -1,8 +1,70 @@
+// // const express = require('express');
+// // const router = express.Router();
+// // const vendorAuth = require('../middleware/vendorAuth');
+// // const {
+// //   setupWarehouse,
+// //   getWarehouseInfo,
+// //   updateWarehouse,
+// //   testParcelXConnection
+// // } = require('../controllers/vendorWarehouse.controller');
+
+// // // All routes require vendor authentication
+// // router.use(vendorAuth);
+
+// // // Setup vendor warehouse
+// // router.post('/setup', setupWarehouse);
+
+// // // Get warehouse info
+// // router.get('/info', getWarehouseInfo);
+
+// // // Update warehouse
+// // router.put('/update', updateWarehouse);
+
+// // // Test ParcelX connection
+// // router.get('/test-connection', testParcelXConnection);
+
+// // module.exports = router;
 
 
+
+// const express = require('express');
+// const router = express.Router();
+// const vendorAuth = require('../middleware/vendorAuth');
+// const {
+//   setupWarehouse,
+//   getWarehouseInfo,
+//   updateWarehouse,
+//   testParcelXConnection,
+//   getAllVendorsWarehouseStatus,
+//   deleteWarehouse
+// } = require('../controllers/vendorWarehouse.controller');
+
+// // All routes require vendor authentication
+// router.use(vendorAuth);
+
+// // Setup vendor warehouse
+// router.post('/setup', setupWarehouse);
+
+// // Get warehouse info
+// router.get('/info', getWarehouseInfo);
+
+// // Update warehouse
+// router.put('/update', updateWarehouse);
+
+// // Delete warehouse
+// router.delete('/delete', deleteWarehouse);
+
+// // Test ParcelX connection
+// router.get('/test-connection', testParcelXConnection);
+
+// // Admin: Get all vendors warehouse status
+// router.get('/admin/all-status', getAllVendorsWarehouseStatus);
+
+// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const vendorAuth = require('../middleware/vendorAuth');
+const adminAuth = require('../middleware/adminAuth');
 const {
   setupWarehouse,
   getWarehouseInfo,
@@ -12,7 +74,7 @@ const {
   deleteWarehouse
 } = require('../controllers/vendorWarehouse.controller');
 
-// All routes require vendor authentication
+// Vendor authenticated routes
 router.use(vendorAuth);
 
 // Setup vendor warehouse
@@ -31,6 +93,6 @@ router.delete('/delete', deleteWarehouse);
 router.get('/test-connection', testParcelXConnection);
 
 // Admin: Get all vendors warehouse status
-router.get('/admin/all-status', getAllVendorsWarehouseStatus);
+router.get('/admin/all-status', adminAuth, getAllVendorsWarehouseStatus);
 
 module.exports = router;
