e749b1c44d1f89edaa73b29f86187ad1a8131bed
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-16 19:05:22 +0530

    Update customerOrder.routes.js

--- routes/customerOrder.routes.js
+++ routes/customerOrder.routes.js
@@ -45,45 +45,10 @@
 
 
 
-// const express = require("express");
-// const router = express.Router();
-
-// const userAuth = require("../middleware/userAuth"); // ✅ USE THIS
-
-// const {
-//   createOrder,
-//   getCustomerOrders,
-//   getOrderById,
-//   getMyOrders,
-//   cancelOrder,
-//   updateOrderStatus,
-//   getVendorOrders,
-//   getAllOrdersForAdmin,
-//   deleteOrder,
-// } = require("../controllers/customerOrder.controller");
-
-// router.post("/order/create", createOrder);
-// router.get("/orders/:customerId", getCustomerOrders);
-
-// // 🔐 LOGGED-IN USER ORDERS
-// router.get("/my-orders", userAuth, getMyOrders);
-
-// router.get("/order/:id", getOrderById);
-// router.put("/order/cancel/:id", cancelOrder);
-// router.put("/order/status/:id", updateOrderStatus);
-
-// router.get("/vendor-orders/:vendorId", getVendorOrders);
-// router.get("/admin/orders", getAllOrdersForAdmin);
-// router.delete("/order/:id", deleteOrder);
-
-// module.exports = router;
-
-
-
 const express = require("express");
 const router = express.Router();
 
-const userAuth = require("../middleware/userAuth");
+const userAuth = require("../middleware/userAuth"); // ✅ USE THIS
 
 const {
   createOrder,
@@ -112,5 +77,4 @@ router.get("/admin/orders", getAllOrdersForAdmin);
 router.delete("/order/:id", deleteOrder);
 
 module.exports = router;
-
 
