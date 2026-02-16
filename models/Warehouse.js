952b474e54c4ee0f045e30ef46eb200f7a6edcbf
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-16 16:25:01 +0530

    Create Warehouse.js

---
+++ models/Warehouse.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const WarehouseSchema = new mongoose.Schema(
+  {
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+
+    address_title: String,
+    sender_name: String,
+    full_address: String,
+    phone: String,
+    pincode: String,
+
+    pick_address_id: {
+      type: Number, // ParcelX
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Warehouse", WarehouseSchema);
