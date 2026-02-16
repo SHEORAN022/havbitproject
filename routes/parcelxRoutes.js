2a0f029462fc5ae6cdc9ad27387e0bcd8ed0d7d0
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-16 14:01:24 +0530

    Update parcelxRoutes.js

--- routes/parcelxRoutes.js
+++ routes/parcelxRoutes.js
@@ -1,3 +1,19 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const {
+//   createWarehouse,
+//   createOrder,
+//   trackOrder,
+//   shipmentDetails,
+// } = require("../controllers/parcelxController");
+
+// router.post("/warehouse", createWarehouse);
+// router.post("/order", createOrder);
+// router.get("/order/track", trackOrder);
+// router.get("/shipment", shipmentDetails);
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
 
@@ -6,9 +22,11 @@ const {
   createOrder,
   trackOrder,
   shipmentDetails,
+   getCourierRates,  
 } = require("../controllers/parcelxController");
 
 router.post("/warehouse", createWarehouse);
+router.get("/courier/rates", getCourierRates);
 router.post("/order", createOrder);
 router.get("/order/track", trackOrder);
 router.get("/shipment", shipmentDetails);
