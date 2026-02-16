a21bb36372c008a0f998d78895e94c22e7d74a96
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-16 16:28:48 +0530

    Update parcelxRoutes.js

--- routes/parcelxRoutes.js
+++ routes/parcelxRoutes.js
@@ -6,29 +6,29 @@
 //   createOrder,
 //   trackOrder,
 //   shipmentDetails,
+//    getCourierRates,  
 // } = require("../controllers/parcelxController");
 
 // router.post("/warehouse", createWarehouse);
+// router.post("/courier/rates", getCourierRates);
 // router.post("/order", createOrder);
 // router.get("/order/track", trackOrder);
 // router.get("/shipment", shipmentDetails);
 
 // module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 
-const {
-  createWarehouse,
-  createOrder,
-  trackOrder,
-  shipmentDetails,
-   getCourierRates,  
-} = require("../controllers/parcelxController");
+const parcelxCtrl = require("../controllers/parcelxController");
 
-router.post("/warehouse", createWarehouse);
-router.post("/courier/rates", getCourierRates);
-router.post("/order", createOrder);
-router.get("/order/track", trackOrder);
-router.get("/shipment", shipmentDetails);
+router.post("/warehouse", parcelxCtrl.createWarehouse);
+router.get("/warehouses/:vendorId", parcelxCtrl.getVendorWarehouses);
+
+router.post("/customer/order", parcelxCtrl.createCustomerOrder);
+
+router.get("/order/track", parcelxCtrl.trackOrder);
+router.get("/shipment", parcelxCtrl.shipmentDetails);
+router.post("/courier/rates", parcelxCtrl.getCourierRates);
 
 module.exports = router;
