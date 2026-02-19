97c81df37d270218319d17c6bc1254066669570a
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-19 14:47:46 +0530

    Update Warehouse.js

--- models/Warehouse.js
+++ models/Warehouse.js
@@ -1,27 +1,25 @@
 const mongoose = require("mongoose");
 
-const warehouseSchema = new mongoose.Schema(
-  {
-    vendorId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Vendor",
-      required: true,
-    },
-
-    shiprocketWarehouseId: {
-      type: String,
-      required: true,
-    },
-
-    warehouseName: String,
-    email: String,
-    phone: String,
-    city: String,
-    state: String,
-    pincode: String,
-    address: String,
+const warehouseSchema = new mongoose.Schema({
+  vendorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Vendor",
+    required: true
   },
-  { timestamps: true }
-);
+
+  address_title: { type: String, required: true },
+  sender_name: { type: String, required: true },
+  full_address: { type: String, required: true },
+  phone: { type: String, required: true },
+
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  pincode: { type: String, required: true },
+  country: { type: String, default: "India" },
+
+  pick_address_id: { type: String, required: true }
+}, { timestamps: true });
+
+warehouseSchema.index({ vendorId: 1 });
 
 module.exports = mongoose.model("Warehouse", warehouseSchema);
