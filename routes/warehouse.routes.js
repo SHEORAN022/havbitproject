781fbb43b1acc3afc53053e20060ef1ecfc7e242
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-19 14:50:39 +0530

    Update warehouse.routes.js

--- routes/warehouse.routes.js
+++ routes/warehouse.routes.js
@@ -1,8 +1,16 @@
+// const express = require("express");
+// const router = express.Router();
+// const { createWarehouse } = require("../controllers/warehouse.controller");
+// const vendorAuth = require("../middleware/vendorAuth");
+
+// router.post("/create", vendorAuth, createWarehouse);
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/vendorAuth");
 const { createWarehouse } = require("../controllers/warehouse.controller");
-const vendorAuth = require("../middleware/vendorAuth");
 
-router.post("/create", vendorAuth, createWarehouse);
+router.post("/warehouse", auth, createWarehouse);
 
 module.exports = router;
