00932757743a26ce907a8292414d64735418b546
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-24 16:34:25 +0530

    Update parcelxController.js

--- controllers/parcelxController.js
+++ controllers/parcelxController.js
@@ -575,3 +575,24 @@ exports.createWarehouse = async (req, res) => {
     });
   }
 };
+exports.getVendorWarehouses = async (req, res) => {
+  try {
+    const { vendorId } = req.params;
+
+    const warehouses = await Warehouse.find({ vendorId }).sort({
+      createdAt: -1,
+    });
+
+    return res.json({
+      success: true,
+      count: warehouses.length,
+      warehouses,
+    });
+  } catch (error) {
+    console.error("GET WAREHOUSE ERROR:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
