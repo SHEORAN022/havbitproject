ae63cceeb9c09199cf54491bb6dd56ed90a79a4f
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-24 13:40:52 +0530

    Update Warehouse.js

--- models/Warehouse.js
+++ models/Warehouse.js
@@ -1,71 +1,105 @@
+// // const mongoose = require("mongoose");
+
+// // const warehouseSchema = new mongoose.Schema({
+// //   vendorId: {
+// //     type: mongoose.Schema.Types.ObjectId,
+// //     ref: "Vendor",
+// //     required: true
+// //   },
+
+// //   address_title: { type: String, required: true },
+// //   sender_name: { type: String, required: true },
+// //   full_address: { type: String, required: true },
+// //   phone: { type: String, required: true },
+
+// //   city: { type: String, required: true },
+// //   state: { type: String, required: true },
+// //   pincode: { type: String, required: true },
+// //   country: { type: String, default: "India" },
+
+// //   pick_address_id: { type: String, required: true }
+// // }, { timestamps: true });
+
+// // warehouseSchema.index({ vendorId: 1 });
+
+// // module.exports = mongoose.model("Warehouse", warehouseSchema);
 // const mongoose = require("mongoose");
 
-// const warehouseSchema = new mongoose.Schema({
-//   vendorId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Vendor",
-//     required: true
+// const WarehouseSchema = new mongoose.Schema(
+//   {
+//     vendorId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Vendor",
+//       required: true,
+//       index: true
+//     },
+//     address_title: {
+//       type: String,
+//       required: true
+//     },
+//     sender_name: {
+//       type: String,
+//       required: true
+//     },
+//     full_address: {
+//       type: String,
+//       required: true
+//     },
+//     phone: {
+//       type: String,
+//       required: true
+//     },
+//     pincode: {
+//       type: String,
+//       required: true
+//     },
+//     pick_address_id: {
+//       type: Number,
+//       required: true,
+//       unique: true
+//     },
+//     is_active: {
+//       type: Boolean,
+//       default: true
+//     }
 //   },
+//   {
+//     timestamps: true
+//   }
+// );
 
-//   address_title: { type: String, required: true },
-//   sender_name: { type: String, required: true },
-//   full_address: { type: String, required: true },
-//   phone: { type: String, required: true },
+// module.exports = mongoose.model("Warehouse", WarehouseSchema);
 
-//   city: { type: String, required: true },
-//   state: { type: String, required: true },
-//   pincode: { type: String, required: true },
-//   country: { type: String, default: "India" },
 
-//   pick_address_id: { type: String, required: true }
-// }, { timestamps: true });
-
-// warehouseSchema.index({ vendorId: 1 });
-
-// module.exports = mongoose.model("Warehouse", warehouseSchema);
 const mongoose = require("mongoose");
 
-const WarehouseSchema = new mongoose.Schema(
+const warehouseSchema = new mongoose.Schema(
   {
     vendorId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Vendor",
+      type: String,
       required: true,
-      index: true
+      index: true,
     },
-    address_title: {
+
+    parcelxWarehouseId: {
       type: String,
-      required: true
-    },
-    sender_name: {
-      type: String,
-      required: true
-    },
-    full_address: {
-      type: String,
-      required: true
-    },
-    phone: {
-      type: String,
-      required: true
-    },
-    pincode: {
-      type: String,
-      required: true
-    },
-    pick_address_id: {
-      type: Number,
       required: true,
-      unique: true
+      unique: true,
     },
-    is_active: {
-      type: Boolean,
-      default: true
-    }
+
+    name: { type: String, required: true },
+
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    pincode: { type: String, required: true },
+
+    contactPerson: String,
+    phone: { type: String, required: true },
+
+    isActive: { type: Boolean, default: true },
   },
-  {
-    timestamps: true
-  }
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("Warehouse", WarehouseSchema);
+module.exports = mongoose.model("Warehouse", warehouseSchema);
