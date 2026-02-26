84a9ad753614fcb513af941f8defc7b8449f3b15
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-02-26 11:25:34 +0530

    Update parcelxRoutes.js

--- routes/parcelxRoutes.js
+++ routes/parcelxRoutes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const ctrl = require("../controllers/parcelxController");
 
 // Warehouse APIs
-router.get("/debug-test", ctrl.debugTest); 
+
 router.post("/warehouse", ctrl.createWarehouse);
 router.get("/warehouse/:vendorId", ctrl.getVendorWarehouses);
 router.post("/order", ctrl.createParcelxOrder);
