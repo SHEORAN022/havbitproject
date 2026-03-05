a49fd737aef077bf4bc32b0f866e017d94d3cb2b
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-03-05 16:36:55 +0530

    Update parcelxRoutes.js

--- routes/parcelxRoutes.js
+++ routes/parcelxRoutes.js
@@ -19,7 +19,7 @@ router.get("/warehouse", userAuth, ctrl.getWarehouseForCustomer);
    ORDER ROUTES
 ================================ */
 router.post("/order", userAuth, ctrl.createParcelxOrder);
-router.get("/orders", vendorAuth, ctrl.getAllOrders);
+router.get("/orders",  ctrl.getAllOrders);
 router.get("/track/:awb", ctrl.trackParcelxOrder);
 router.get("/shipment/:awb", ctrl.getParcelxShipmentDetails);
 
