c9d7b839235cfb023d0dbd3d65981afa449ed110
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-03-06 16:57:38 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,3 +1,23 @@
+// // const mongoose = require("mongoose");
+
+// // const userSchema = new mongoose.Schema(
+// //   {
+// //     name: { type: String, required: true },
+// //     email: { type: String, required: true, unique: true },
+// //     password: { type: String, required: true },
+// //     role: { type: String, enum: ["user", "admin"], default: "user" },
+// //     image: { type: String }, // profile picture URL (Cloudinary)
+// //   },
+// //   { timestamps: true }
+// // );
+
+// // module.exports = mongoose.model("User", userSchema);
+
+
+
+
+
+
 // const mongoose = require("mongoose");
 
 // const userSchema = new mongoose.Schema(
@@ -18,6 +38,7 @@
 
 
 
+
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
@@ -32,4 +53,5 @@ const userSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("User", userSchema);
+
 
