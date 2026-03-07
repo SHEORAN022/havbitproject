62fb5b169ef3560ddf83a9b196b982b9a99bb333
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-03-07 12:59:47 +0530

    Create uploads.js

---
+++ middleware/uploads.js
@@ -0,0 +1,39 @@
+const multer = require("multer");
+const path = require("path");
+
+/* STORAGE CONFIG */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // uploads folder
+  },
+
+  filename: function (req, file, cb) {
+    const uniqueName =
+      Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+    cb(null, uniqueName + path.extname(file.originalname));
+  },
+});
+
+/* FILE FILTER */
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype.startsWith("image/") ||
+    file.mimetype === "application/pdf"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only Images & PDFs allowed"), false);
+  }
+};
+
+/* MULTER INSTANCE */
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10MB
+  },
+});
+
+module.exports = upload;
