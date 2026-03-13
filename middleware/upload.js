5fb783f4c4527648d884c76c178f6e7a6c9a0325
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-03-13 12:57:31 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,29 +1,3 @@
-
-
-// // module.exports = upload;
-// const multer = require("multer");
-
-// const storage = multer.memoryStorage();
-
-// const fileFilter = (req, file, cb) => {
-//   if (file.mimetype.startsWith("image/")) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error("Only image files are allowed"), false);
-//   }
-// };
-
-// const upload = multer({
-//   storage,
-//   fileFilter,
-//   limits: {
-//     fileSize: 5 * 1024 * 1024,
-//   },
-// });
-
-// module.exports = upload;
-
-
 const multer = require("multer");
 const path = require("path");
 
