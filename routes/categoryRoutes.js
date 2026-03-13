d516f1ad16684fe3fa307b1243e35dad4d7d7e23
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-03-13 13:11:40 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,35 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const cloudUpload = require("../middleware/cloudUpload");
-// const Category = require("../models/Category");
-// const {
-//   createCategory,
-//   getCategories,
-//   getCategoryById,
-//   updateCategory,
-//   deleteCategory
-// } = require("../controllers/categoryController");
-
-// // CREATE
-// router.post("/", cloudUpload.single("image"), createCategory);
-
-// // READ ALL
-// router.get("/", getCategories);
-
-// // READ SINGLE
-// router.get("/:id", getCategoryById);
-
-// // UPDATE
-// router.put("/:id", cloudUpload.single("image"), updateCategory);
-
-// // DELETE
-// router.delete("/:id", deleteCategory);
-
-// module.exports = router;
-
-
-
-
 const express = require("express");
 const router = express.Router();
 const cloudUpload = require("../middleware/cloudUpload");
