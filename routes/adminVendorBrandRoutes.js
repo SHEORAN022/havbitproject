412533c824192a20c1bb1836c3b92aed24035d75
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-03-13 13:10:56 +0530

    Update adminVendorBrandRoutes.js

--- routes/adminVendorBrandRoutes.js
+++ routes/adminVendorBrandRoutes.js
@@ -1,33 +1,3 @@
-// const router = require("express").Router();
-// const upload = require("../middleware/upload");
-// const adminAuth = require("../middleware/adminAuth");
-
-// const {
-//   getAllVendorBrands,
-//   getBrandByVendorId,
-//   updateBrandByVendorId,
-// } = require("../controllers/vendorBrandController");
-
-// router.get("/vendor-brands", adminAuth, getAllVendorBrands);
-
-// router.get("/vendor-brands/:vendorId", adminAuth, getBrandByVendorId);
-
-// router.put(
-//   "/vendor-brands/:vendorId",
-//   adminAuth,
-//   upload.fields([
-//     { name: "logo", maxCount: 1 },
-//     { name: "banner", maxCount: 1 },
-//   ]),
-//   updateBrandByVendorId
-// );
-
-// module.exports = router;
-
-
-
-
-
 const express = require("express");
 const router = express.Router();
 
