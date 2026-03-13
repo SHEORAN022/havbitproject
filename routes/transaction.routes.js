d6e2aa7840e17de7330843871d9d9c8d0c367b8e
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-03-13 13:15:20 +0530

    Update transaction.routes.js

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,24 +1,3 @@
-
-
-// const express = require("express");
-// const router = express.Router();
-
-// const {
-//   getCustomerTransactions,
-//   getAllTransactionsForAdmin,
-// } = require("../controllers/transaction.controller");
-
-// const userAuth = require("../middleware/userAuth");
-// const adminAuth = require("../middleware/adminAuth");
-
-// /* CUSTOMER */
-// router.get("/my", userAuth, getCustomerTransactions);
-
-// /* ADMIN */
-// router.get("/admin/all", adminAuth, getAllTransactionsForAdmin);
-
-// module.exports = router;
-
 const express  = require("express");
 const router   = express.Router();
 const ctrl     = require("../controllers/transaction.controller");
