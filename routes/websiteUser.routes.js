363f1006e87f2f3e6c2bde3d10619ec353fa88b4
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-03-13 13:18:40 +0530

    Update websiteUser.routes.js

--- routes/websiteUser.routes.js
+++ routes/websiteUser.routes.js
@@ -1,15 +1,3 @@
-
-// const express = require("express");
-// const router = express.Router();
-// const controller = require("../controllers/websiteUser.controller"); // 👈 SAME NAME
-// const userAuth = require("../middleware/userAuth");
-
-// router.post("/signup", controller.signup);
-// router.post("/login", controller.login);
-// router.get("/all", controller.getAllWebsiteUsers);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/websiteUser.controller");
