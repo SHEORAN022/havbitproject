6adae834b2d85e149def275997d7af58e38e8776
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-03-31 11:12:26 +0530

    Update Review.model.js

--- models/Review.model.js
+++ models/Review.model.js
@@ -1,3 +1,50 @@
+// const mongoose = require("mongoose");
+
+// const reviewSchema = new mongoose.Schema(
+//   {
+//     userName: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+
+//     userEmail: {
+//       type: String,
+//       required: true,
+//       lowercase: true,
+//     },
+
+//     rating: {
+//       type: Number,
+//       required: true,
+//       min: 1,
+//       max: 5,
+//     },
+
+//     comment: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+
+//     status: {
+//       type: String,
+//       enum: ["pending", "approved", "rejected"],
+//       default: "pending",
+//     },
+
+//     source: {
+//       type: String,
+//       enum: ["website", "app"],
+//       default: "website",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Review", reviewSchema);
+
+
 const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema(
@@ -26,6 +73,13 @@ const reviewSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+
+    // ✅ ADD THIS
+    gallery: [
+      {
+        type: String,
+      },
+    ],
 
     status: {
       type: String,
